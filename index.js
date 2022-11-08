6af66024a37cb9ffd06bb9a81da0a8a80e0b3c96
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-11-08 16:00:58 +0600

    checkout api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 
@@ -24,11 +24,19 @@ async function run() {
             const services = await cursor.limit(3).toArray();
             res.send(services);
         })
+
         app.get('/allServices', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
     }
     finally {
