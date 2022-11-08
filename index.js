755220c879a29173c07d4eb85cef87b2cd6ffa8d
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-11-08 12:50:30 +0600

    mongodb user added

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
+
+
 
 
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xfjdabp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
     res.send('API is running');
+});
+
+app.get('/services', async (req, res) => {
+
 })
 
 app.listen(port, () => console.log(`API is running on ${port}`))
