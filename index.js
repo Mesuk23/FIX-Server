16a9f5788179ae6facbf5019141c9ca972c7e52e
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-11-09 02:30:11 +0600

    post method added

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('mobile-services').collection('services');
+        const reviewCollection = client.db('mobile-services').collection('reviews');
+
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -37,6 +39,12 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        })
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
         })
     }
     finally {
