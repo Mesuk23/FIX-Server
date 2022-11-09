e483a8775d9cd166f3c1f7d9c9058a972e4211f9
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-11-09 13:08:08 +0600

    reviews by name api added

--- index.js
+++ index.js
@@ -48,7 +48,24 @@ async function run() {
         })
 
         app.get('/reviews', async (req, res) => {
-            const query = {};
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        app.get('/service-reviews', async (req, res) => {
+            let query = {};
+            if (req.query.service) {
+                query = {
+                    service: req.query.service
+                }
+            }
             const cursor = reviewCollection.find(query);
             const reviews = await cursor.toArray();
             res.send(reviews);
